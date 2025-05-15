3745aed861aa10c8a9fbbcfe46739a6e5ad38196
Raymond <raymond.runoi@gmail.com>
2025-05-15 13:18:06 +0300

    Test: Add tests for division function.

--- tests/test_calculations.c
+++ tests/test_calculations.c
@@ -81,8 +81,35 @@ static void test_multiply_one_negative(void)
 
 static void test_multiply_two_negative(void)
 {
-  // TEST_IGNORE();
+  TEST_IGNORE();
   TEST_ASSERT_EQUAL_INT(9, multiply(-3, -3));
+}
+
+/**
+ * Tests for the division function
+ */
+static void test_divide(void)
+{
+  // TEST_IGNORE();
+  TEST_ASSERT_EQUAL_INT(3, divide(9, 3));
+}
+
+static void test_divide_zero(void)
+{
+  // TEST_IGNORE();
+  TEST_ASSERT_EQUAL_INT(-1, divide(3, 0));
+}
+
+static void test_divide_one_negative(void)
+{
+  // TEST_IGNORE();
+  TEST_ASSERT_EQUAL_INT(-3, divide(9, -3));
+}
+
+static void test_divide_two_negative(void)
+{
+  // TEST_IGNORE();
+  TEST_ASSERT_EQUAL_INT(3, divide(-9, -3));
 }
 
 int main(void)
@@ -106,6 +133,12 @@ int main(void)
   RUN_TEST(test_multiply_zero);
   RUN_TEST(test_multiply_one_negative);
   RUN_TEST(test_multiply_two_negative);
+
+  // Run division tests
+  RUN_TEST(test_divide);
+  RUN_TEST(test_divide_zero);
+  RUN_TEST(test_divide_one_negative);
+  RUN_TEST(test_divide_two_negative);
 
   return UNITY_END();
 }
